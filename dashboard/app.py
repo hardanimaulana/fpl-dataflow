41c4afce6093e96afa6bf38df55b65f57bb1fdb7
Hardani Maulana <33679846+hardanimaulana@users.noreply.github.com>
2025-09-01 00:04:48 +0200

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,6 +7,6 @@ df_draft = con.execute("SELECT * FROM draft_standings").df()
 # df_classic = con.execute("SELECT * FROM classic_standings").df()
 # df_players = con.execute("SELECT * FROM players_stats").df()
 
-st.write("Draft standings", df_draft.tail())
+st.write("Draft standings", df_draft)
 # st.write("Classic standings", df_classic.tail())
 # st.write("Players", df_players.head())
